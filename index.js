1ea97f3481e19e3713c9dfa29e374cf595fd52f5
calebredd <caleb.redd@yahoo.com>
2019-10-30 15:30:05 -0700

    Initial server rendering setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code away!
+
+const express = require("express"),
+  server = require("./server"),
+  app = express();
+
+app.use(server);
+app.use(express.json());
+
+app.listen(9000, () => {
+  console.log("Server running on localhost:9000");
+});
