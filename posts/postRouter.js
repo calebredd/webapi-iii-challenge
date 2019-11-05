6cdfda8c59a053e6bfa8211517323dfa290dc28c
Caleb Redd <caleb.redd@yahoo.com>
2019-11-04 22:18:25 -0700

    Updated Routes for postRouter. Merged parent into child routes

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,17 +1,70 @@
 const express = require("express");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const Posts = require("./postDb");
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  Posts.get()
+    .then(posts => {
+      res.status(200).json({ posts });
+    })
+    .catch(() =>
+      res.status(500).json({ errorMessage: "Unable to access database." })
+    );
+});
 
-router.get("/:id", (req, res) => {});
+router.get("/:postId", validatePostId, (req, res) => {
+  Posts.getById(req.params.postId)
+    .then(post => {
+      res.status(200).json({ post });
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to get that post." });
+    });
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:postId", validatePostId, (req, res) => {
+  Posts.remove(req.params.postId)
+    .then(post => {
+      res.status(202).json({ post });
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to remove post." });
+    });
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:postId", validatePostId, (req, res) => {
+  const user_id = req.params.id;
+  req.body.user_id = user_id;
+  const editPost = req.body;
+  Posts.update(req.params.id, editPost)
+    .then(post => {
+      if (post) {
+        res.status(201).json(post);
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: "Unable to add that post to that user." });
+    });
+});
 
 // custom middleware
 
-function validatePostId(req, res, next) {}
+function validatePostId(req, res, next) {
+  Posts.getById(req.params.postId)
+    .then(post => {
+      if (post.user_id == req.params.id) {
+        next();
+      } else {
+        res
+          .status(500)
+          .json({ errorMessage: "That post does not belong to that user." });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to get that post." });
+    });
+}
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,174 @@
 const express = require("express");
 const router = express.Router();
-const Users=require("./userDb");
-const postRouter = require("../posts/postRouter");
-router.post("/", (req, res) => {});
-
-// router.post('/:id/posts', (req, res) => {
-
-// });
+const Users = require("./userDb");
+const Posts = require("../posts/postDb");
+// const postRouter = require("../posts/postRouter");
 
 router.get("/", (req, res) => {
-  res.send("<h2>Hello I'm a user</h2>")
+  Users.get()
+    .then(users => {
+      res.status(200).json({ users });
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to access database." });
+    });
 });
 
-router.get("/:id", (req, res) => {});
+router.post("/", validateUser, (req, res) => {
+  const newUser = req.body;
 
-// router.get('/:id/posts', (req, res) => {
+  Users.insert(newUser)
+    .then(user => {
+      if (user) {
+        res.status(201);
+        Users.get()
+          .then(users => {
+            res.status(200).json(users);
+          })
+          .catch(() => {
+            res
+              .status(500)
+              .json({ errorMessage: "Unable to access database." });
+          });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to add user." });
+    });
+});
 
-// });
+router.get("/:id", validateUserId, (req, res) => {
+  Users.getById(req.params.id)
+    .then(user => {
+      // console.log(user.name);
+      // console.log({ user });
+      res.status(200).json({ user });
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to find that User." });
+    });
+});
 
-router.delete("/:id", (req, res) => {});
+router.get("/:id/posts", validateUserId, validateUserPosts, (req, res) => {
+  Users.getUserPosts(req.params.id)
+    .then(userPosts => {
+      res.status(200).json({ userPosts });
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: "Unable to find that User's posts." });
+    });
+});
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
+  const user_id = req.params.id;
+  req.body.user_id = user_id;
+  const addPost = req.body;
+  Posts.insert(addPost)
+    .then(post => {
+      if (post) {
+        res.status(201);
+        Users.getUserPosts(req.params.id)
+          .then(userPosts => {
+            res.status(200).json(userPosts);
+          })
+          .catch(() => {
+            res
+              .status(500)
+              .json({ errorMessage: "Unable to find that User's posts." });
+          });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: "Unable to add that post to that user." });
+    });
+});
 
-router.put("/:id", (req, res) => {});
-router.use("/:id/posts", postRouter);
+router.put("/:id", validateUserId, (req, res) => {
+  const editUser = req.body;
+  console.log(editUser);
+  Users.update(req.params.id, editUser)
+    .then(user => {
+      res.status(201);
+      Users.getById(req.params.id)
+        .then(user => {
+          // console.log(user.name);
+          // console.log({ user });
+          res.status(200).json(user);
+        })
+        .catch(() => {
+          res.status(500).json({ errorMessage: "Unable to find that User." });
+        });
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to update user." });
+    });
+});
+
+router.delete("/:id", validateUserId, (req, res) => {
+  Users.remove(req.params.id)
+    .then(user => {
+      res.status(202).json({ user });
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to remove user." });
+    });
+});
 //custom middleware
 
-function validateUserId(req, res, next) {}
+function validateUserId(req, res, next) {
+  Users.getById(req.params.id)
+    .then(user => {
+      if (user.id) {
+        next();
+      } else {
+        res.status(500).json({
+          errorMessage: "Unable to find that User due to Middleware checking."
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: "Unable to find that User due to Middleware checking."
+      });
+    });
+}
 
-function validateUser(req, res, next) {}
+function validateUserPosts(req, res, next) {
+  Users.getUserPosts(req.params.id)
+    .then(userPosts => {
+      if (userPosts.length > 0) {
+        next();
+      } else {
+        res.status(500).json({
+          errorMessage: "That user does not have any posts."
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: "That user does not have any posts."
+      });
+    });
+}
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+  const newPost = req.body;
+  if (!newPost.text) {
+    res.status(404).send("Unable to add post without text");
+  } else {
+    next();
+  }
+}
+function validateUser(req, res, next) {
+  const newUser = req.body;
+  if (!newUser.text) {
+    res.status(404).send("Unable to add user without a Name");
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
