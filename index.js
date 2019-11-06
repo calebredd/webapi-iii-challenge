9466ab0ac63cf3f970f31aaadaf51895417b7a6c
Caleb Redd <caleb.redd@yahoo.com>
2019-11-05 21:03:32 -0700

    Styling for cards

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require("express"),
   logger = require("./middleware/logger"),
   server = express(),
   apiRoutes = require("./routes");
+cors = require("cors");
 server.use(express.json());
 server.use(logger);
+server.use(cors());
 server.get("/", (req, res) => {
   res.send(`<h2>Begin by accessing <a href="./api">/api</a></h2>`);
 });
