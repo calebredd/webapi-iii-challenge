deb7f35398d4de63231aa250d883774d0fa91aeb
Caleb Redd <caleb.redd@yahoo.com>
2019-11-05 21:19:50 -0700

    Added form

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,44 @@ import Posts from "./components/Posts.js";
 function App() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
-    async function fetchData() {
-      await axios
-        .get("http://localhost:9000/api/users")
-        .then(res => {
-          // console.log(res.data.users);
-          setUsers(res.data.users);
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    }
-    fetchData();
+    // async function fetchData() {
+    // await axios
+    axios
+      .get("http://localhost:9000/api/users")
+      .then(res => {
+        // console.log(res.data.users);
+        setUsers(res.data.users);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    // }
+    // fetchData();
   }, []);
+  const submit = e => {
+    e.preventDefault();
+    // console.log(e.target.name.value);
+    const name = e.target.name.value;
+    axios
+      .post("http://localhost:9000/api/users/", { name: name })
+      .then(res => {
+        // console.log(res.data);
+        setUsers(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    e.target.reset();
+  };
   // console.log(users);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Users and Posts API</h1>
+        <form onSubmit={e => submit(e)}>
+          <input type="text" name="name" placeholder="Character Name" />
+          <button type="submit">Add Character</button>
+        </form>
       </header>
       <div className="users">
         {users.map(user => (
